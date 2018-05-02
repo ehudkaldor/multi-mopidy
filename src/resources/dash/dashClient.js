2767021ad57ac660ed29e654d46fb1b2f047f31f
Ehud Kaldor <ehud@unfairfunction.org>
2018-05-02 09:17:01 -0700

    dashboard and dashboard elements

---
+++ src/resources/dash/dashClient.js
@@ -0,0 +1,20 @@
+import { bindable } from 'aurelia-framework'
+
+export class DashClient {
+  // imageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Rami_Malek_in_Hollywood%2C_California.jpg/400px-Rami_Malek_in_Hollywood%2C_California.jpg"
+  orientation = "vertical"
+  controlSidebar = ".seven.wide.column"
+  button = ".ui.button"
+  progress = 10
+  @bindable client
+
+
+  attached() {
+    console.debug("attached dash-client. client: " + JSON.stringify(this.client))
+    // $('.ui.sidebar')
+    // .sidebar({
+    //   context: $('.pushable')
+    // })
+    // .sidebar('attach events', '.eight.wide.violet.column')
+  }
+}
