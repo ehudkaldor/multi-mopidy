6d05f8b6bc6d49cf0e2897b9556be7bb683274c4
Ehud Kaldor <ehud@unfairfunction.org>
2018-05-02 08:59:47 -0700

    partial elements now part of global namespace

--- src/resources/index.js
+++ src/resources/index.js
@@ -1,3 +1,17 @@
 export function configure(config) {
-  //config.globalResources([]);
+  config.globalResources([
+    PLATFORM.moduleName('./elements/sProgressCustomElement'),
+    PLATFORM.moduleName('./dash/dashboard'),
+    PLATFORM.moduleName('./dash/dashClient'),
+    PLATFORM.moduleName('./dash/dashControls'),
+    PLATFORM.moduleName('./dash/dashDetails'),
+    PLATFORM.moduleName('./dash/dashPlaylist'),
+    PLATFORM.moduleName('./dash/dashSearch'),
+    PLATFORM.moduleName('./dash/dashSong'),
+    PLATFORM.moduleName('./dash/dashTracklist'),
+    PLATFORM.moduleName('./dash/dashVolume'),
+    PLATFORM.moduleName('./modals/addClientModal'),
+    PLATFORM.moduleName('./attributes/popupCustomAttribute'),
+    PLATFORM.moduleName('./nav/nav')
+  ]);
 }
