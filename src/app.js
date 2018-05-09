f194a3a3ef42f629d4ddae00437228a8ae9d52d1
Ehud Kaldor <ehud@unfairfunction.org>
2018-05-08 20:44:51 -0700

    added route for broadcasters

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import {PLATFORM} from 'aurelia-pal';
 
 export class App {
+
   configureRouter(config, router) {
     config.title = 'Aurelia';
     config.map([
       // { route: '',          name: 'home',         moduleId: PLATFORM.moduleName('./home/home'),       nav: true, title: 'Home' },
-      { route: ['','sizer'],   name: 'sizer',      moduleId: PLATFORM.moduleName('./sizer/sizer'),           nav: true, title: 'Sizer' },
-      { route: 'elements',     name: 'elements',   moduleId: PLATFORM.moduleName('./elements/elements'),     nav: true, title: 'Elements' }
+      { route: ['','home'],  name: 'home',      moduleId: PLATFORM.moduleName('./pages/home/home'),           nav: true, title: 'Home' },
+      { route: 'clients',     name: 'clients',   moduleId: PLATFORM.moduleName('./pages/clients/clients'),     nav: true, title: 'Clients' },
+      { route: 'broadcasters',     name: 'broadcasters',   moduleId: PLATFORM.moduleName('./pages/broadcasters/broadcasters'),     nav: true, title: 'Broadcasters' }
     ]);
 
     this.router = router;
