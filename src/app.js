82aaa485b076254fbc3f0ace41a1186e62c32df4
Ehud Kaldor <ehud@unfairfunction.org>
2018-05-23 23:05:26 -0700

    added route for individual broadcaster control

--- src/app.js
+++ src/app.js
@@ -8,7 +8,8 @@ export class App {
       // { route: '',          name: 'home',         moduleId: PLATFORM.moduleName('./home/home'),       nav: true, title: 'Home' },
       { route: ['','home'],  name: 'home',      moduleId: PLATFORM.moduleName('./pages/home/home'),           nav: true, title: 'Home' },
       { route: 'clients',     name: 'clients',   moduleId: PLATFORM.moduleName('./pages/clients/clients'),     nav: true, title: 'Clients' },
-      { route: 'broadcasters',     name: 'broadcasters',   moduleId: PLATFORM.moduleName('./pages/broadcasters/broadcasters'),     nav: true, title: 'Broadcasters' }
+      { route: 'broadcasters',     name: 'broadcasters',   moduleId: PLATFORM.moduleName('./pages/broadcasters/broadcasters'),     nav: true, title: 'Broadcasters' },
+      { route: 'broadcasters/:id', name: 'broadcaster',   moduleId: PLATFORM.moduleName('./pages/broadcasters/broadcaster'),     nav: false, title: 'Broadcaster' }
     ]);
 
     this.router = router;
